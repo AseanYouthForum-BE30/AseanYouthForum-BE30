80eec8c047d220156facad242426d6f27ed84f74
Fabyan Kindarya <fkindarya@gmail.com>
2022-10-18 00:40:18 +0700

    Get and Show Article From MockAPI

---
+++ home-page.js
@@ -0,0 +1,38 @@
+const BaseURL = 'https://63497f50a59874146b2192cc.mockapi.io/ayf/'
+
+let articleContainer = document.getElementById('article-container')
+
+let getArticle = async () => {
+    let response = await fetch(BaseURL + 'users/1/articles', {
+        method: 'GET'
+    })
+    
+    let articles = await response.json()
+
+    articleContainer.innerHTML = ''
+
+    articles.forEach(article => {
+
+        let articleElement = document.createElement('div')
+        
+        articleElement.className = 'row mb-3'
+
+        articleElement.innerHTML = `
+            <div class="col d-flex justify-content-center" id="article-image">
+                <img
+                src="${article.image}"
+                class="img-fluid"
+                style="width: 30vw; height: 30vh"
+                alt="" />
+            </div>
+            <div class="col d-flex align-items-center justify-content-center flex-column">
+                <h2 id="article-title">${article.title}</h2>
+                <p id="article-content" style="text-align: justify">${article.content}</p>
+            </div>
+        `
+
+        articleContainer.append(articleElement)
+    });
+}
+
+getArticle()
