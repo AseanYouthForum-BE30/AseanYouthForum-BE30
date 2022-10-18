3eee521e32d6a76d2c81adf4a702eb831e3eb532
irfannudin ihsan <89620323+irfannudinihsan@users.noreply.github.com>
2022-10-18 23:42:01 +0700

    Register page (#4)
    * Add Navbar and Form
    
    * Add Javascript Register
    
    * After Register Page will Reload
    
    * Add Login Href Below Register Button
    
    * Add Loading on Register Button
    
    Co-authored-by: Fabyan Kindarya <fkindarya@gmail.com>

---
+++ register.js
@@ -0,0 +1,37 @@
+let BASE_URL = "https://63497f50a59874146b2192cc.mockapi.io/ayf/"
+
+let nama_element = document.querySelector('#nama_lengkap')
+let email_element = document.querySelector('#email')
+let password_element = document.querySelector('#password')
+
+let button = document.getElementById('button')
+
+let postDataElement = async () => {
+    const name_value = nama_element.value
+    const email_value = email_element.value
+    const password_value = password_element.value
+
+    await fetch(BASE_URL + 'users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            full_name: name_value,
+            email: email_value,
+            password : password_value,
+        })
+    })
+
+    window.location.reload(true)
+}
+
+button.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    button.innerHTML = `
+        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    `
+
+    postDataElement()
+})
